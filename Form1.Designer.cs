db060d1dfa14b9e18dd5a192d3e5456fd1ae28e4
diegoanlima <diegooantoniolima@gmail.com>
2024-04-04 18:08:57 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace AgendaTelefonica
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nome = new System.Windows.Forms.Label();
+            this.ContatosListBox = new System.Windows.Forms.ListBox();
+            this.EditarContatoButton = new System.Windows.Forms.Button();
+            this.ExcluirContatoButton = new System.Windows.Forms.Button();
+            this.NomeTextBox = new System.Windows.Forms.TextBox();
+            this.TelefoneTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.TipoContatoComboBox = new System.Windows.Forms.ComboBox();
+            this.AdicionarContatoButton = new System.Windows.Forms.Button();
+            this.PesquisarTextBox = new System.Windows.Forms.TextBox();
+            this.PesquisarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nome
+            // 
+            this.nome.AutoSize = true;
+            this.nome.ForeColor = System.Drawing.SystemColors.Control;
+            this.nome.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.nome.Location = new System.Drawing.Point(29, 30);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(38, 13);
+            this.nome.TabIndex = 0;
+            this.nome.Text = "Nome;";
+            // 
+            // ContatosListBox
+            // 
+            this.ContatosListBox.FormattingEnabled = true;
+            this.ContatosListBox.Location = new System.Drawing.Point(12, 240);
+            this.ContatosListBox.Name = "ContatosListBox";
+            this.ContatosListBox.Size = new System.Drawing.Size(776, 199);
+            this.ContatosListBox.TabIndex = 1;
+            this.ContatosListBox.SelectedIndexChanged += new System.EventHandler(this.ContatosListBox_SelectedIndexChanged);
+            // 
+            // EditarContatoButton
+            // 
+            this.EditarContatoButton.Location = new System.Drawing.Point(144, 203);
+            this.EditarContatoButton.Name = "EditarContatoButton";
+            this.EditarContatoButton.Size = new System.Drawing.Size(106, 31);
+            this.EditarContatoButton.TabIndex = 3;
+            this.EditarContatoButton.Text = "Editar";
+            this.EditarContatoButton.UseVisualStyleBackColor = true;
+            // 
+            // ExcluirContatoButton
+            // 
+            this.ExcluirContatoButton.Location = new System.Drawing.Point(256, 203);
+            this.ExcluirContatoButton.Name = "ExcluirContatoButton";
+            this.ExcluirContatoButton.Size = new System.Drawing.Size(106, 31);
+            this.ExcluirContatoButton.TabIndex = 4;
+            this.ExcluirContatoButton.Text = "Excluir";
+            this.ExcluirContatoButton.UseVisualStyleBackColor = true;
+            // 
+            // NomeTextBox
+            // 
+            this.NomeTextBox.Location = new System.Drawing.Point(118, 27);
+            this.NomeTextBox.Name = "NomeTextBox";
+            this.NomeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NomeTextBox.TabIndex = 5;
+            // 
+            // TelefoneTextBox
+            // 
+            this.TelefoneTextBox.Location = new System.Drawing.Point(118, 53);
+            this.TelefoneTextBox.Name = "TelefoneTextBox";
+            this.TelefoneTextBox.Size = new System.Drawing.Size(100, 20);
+            this.TelefoneTextBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.label1.Location = new System.Drawing.Point(29, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Numero:";
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(118, 79);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(100, 20);
+            this.EmailTextBox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.label2.Location = new System.Drawing.Point(29, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "EMAIL:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.label3.Location = new System.Drawing.Point(29, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Tipo de Contato:";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(522, 30);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 11;
+            // 
+            // TipoContatoComboBox
+            // 
+            this.TipoContatoComboBox.FormattingEnabled = true;
+            this.TipoContatoComboBox.Items.AddRange(new object[] {
+            "Celular",
+            "Residencial",
+            "Comercial"});
+            this.TipoContatoComboBox.Location = new System.Drawing.Point(118, 105);
+            this.TipoContatoComboBox.Name = "TipoContatoComboBox";
+            this.TipoContatoComboBox.Size = new System.Drawing.Size(100, 21);
+            this.TipoContatoComboBox.TabIndex = 13;
+            // 
+            // AdicionarContatoButton
+            // 
+            this.AdicionarContatoButton.Location = new System.Drawing.Point(144, 135);
+            this.AdicionarContatoButton.Name = "AdicionarContatoButton";
+            this.AdicionarContatoButton.Size = new System.Drawing.Size(75, 23);
+            this.AdicionarContatoButton.TabIndex = 14;
+            this.AdicionarContatoButton.Text = "Salvar";
+            this.AdicionarContatoButton.UseVisualStyleBackColor = true;
+            // 
+            // PesquisarTextBox
+            // 
+            this.PesquisarTextBox.Location = new System.Drawing.Point(480, 209);
+            this.PesquisarTextBox.Name = "PesquisarTextBox";
+            this.PesquisarTextBox.Size = new System.Drawing.Size(200, 20);
+            this.PesquisarTextBox.TabIndex = 15;
+            // 
+            // PesquisarButton
+            // 
+            this.PesquisarButton.Location = new System.Drawing.Point(698, 207);
+            this.PesquisarButton.Name = "PesquisarButton";
+            this.PesquisarButton.Size = new System.Drawing.Size(75, 23);
+            this.PesquisarButton.TabIndex = 16;
+            this.PesquisarButton.Text = "Buscar";
+            this.PesquisarButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PesquisarButton);
+            this.Controls.Add(this.PesquisarTextBox);
+            this.Controls.Add(this.AdicionarContatoButton);
+            this.Controls.Add(this.TipoContatoComboBox);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TelefoneTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NomeTextBox);
+            this.Controls.Add(this.ExcluirContatoButton);
+            this.Controls.Add(this.EditarContatoButton);
+            this.Controls.Add(this.ContatosListBox);
+            this.Controls.Add(this.nome);
+            this.Name = "MainForm";
+            this.Text = "Agenda ";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nome;
+        private System.Windows.Forms.ListBox ContatosListBox;
+        private System.Windows.Forms.Button EditarContatoButton;
+        private System.Windows.Forms.Button ExcluirContatoButton;
+        private System.Windows.Forms.TextBox NomeTextBox;
+        private System.Windows.Forms.TextBox TelefoneTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.ComboBox TipoContatoComboBox;
+        private System.Windows.Forms.Button AdicionarContatoButton;
+        private System.Windows.Forms.TextBox PesquisarTextBox;
+        private System.Windows.Forms.Button PesquisarButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AgendaTelefonica
+{
+    public partial class MainForm : Form
+    {
+        private List<Contato> contatos;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            contatos = new List<Contato>();
+
+            // Vincule o evento Load do formulário ao método MainForm_Load
+            this.Load += MainForm_Load;
+        }
+
+        private void AdicionarContatoButton_Click(object sender, EventArgs e)
+        {
+            // Obter dados do formulário
+            string nome = NomeTextBox.Text.Trim();
+            string telefone = TelefoneTextBox.Text.Trim();
+            string email = EmailTextBox.Text.Trim();
+            string tipo = TipoContatoComboBox.SelectedItem?.ToString();
+
+            // Verificar se todos os campos foram preenchidos
+            if (!string.IsNullOrEmpty(nome) && !string.IsNullOrEmpty(telefone) && !string.IsNullOrEmpty(tipo))
+            {
+                // Criar um novo objeto Contato
+                Contato novoContato = new Contato(nome, telefone, email, tipo);
+
+                // Adicionar o novo contato à lista
+                contatos.Add(novoContato);
+
+                // Atualizar o ListBox
+                AtualizarListBox();
+
+                // Salvar contatos no armazenamento
+                SalvarContatosNoArmazenamento();
+
+                // Limpar os campos do formulário
+                LimparCampos();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, preencha todos os campos.");
+            }
+        }
+
+        private void ExcluirContatoButton_Click(object sender, EventArgs e)
+        {
+            if (ContatosListBox.SelectedItem != null)
+            {
+                int index = ContatosListBox.SelectedIndex;
+                contatos.RemoveAt(index);
+
+                // Salvar contatos no armazenamento
+                SalvarContatosNoArmazenamento();
+
+                AtualizarListBox();
+            }
+            else
+            {
+                MessageBox.Show("Selecione um contato para excluir.");
+            }
+        }
+
+        private void PesquisarButton_Click(object sender, EventArgs e)
+        {
+            string termoPesquisa = PesquisarTextBox.Text.Trim();
+
+            if (!string.IsNullOrEmpty(termoPesquisa))
+            {
+                var contatosFiltrados = contatos.Where(contato =>
+                    contato.Nome.IndexOf(termoPesquisa, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                    contato.Telefone.IndexOf(termoPesquisa, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                    contato.Email.IndexOf(termoPesquisa, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                    contato.Tipo.IndexOf(termoPesquisa, StringComparison.OrdinalIgnoreCase) >= 0
+                ).ToList();
+
+                AtualizarListBox(contatosFiltrados);
+            }
+            else
+            {
+                AtualizarListBox();
+            }
+        }
+
+        private void ContatosListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ContatosListBox.SelectedItem != null)
+            {
+                // Obter o contato selecionado
+                Contato contatoSelecionado = (Contato)ContatosListBox.SelectedItem;
+
+                // Exibir os detalhes do contato selecionado nos TextBoxes
+                NomeTextBox.Text = contatoSelecionado.Nome;
+                TelefoneTextBox.Text = contatoSelecionado.Telefone;
+                EmailTextBox.Text = contatoSelecionado.Email;
+                TipoContatoComboBox.SelectedItem = contatoSelecionado.Tipo;
+            }
+            else
+            {
+                // Se nenhum contato estiver selecionado, limpar os TextBoxes
+                LimparCampos();
+            }
+        }
+
+        private void AtualizarListBox(List<Contato> contatosParaExibir = null)
+        {
+            ContatosListBox.Items.Clear();
+            if (contatosParaExibir == null)
+            {
+                foreach (Contato contato in contatos)
+                {
+                    ContatosListBox.Items.Add(contato);
+                }
+            }
+            else
+            {
+                foreach (Contato contato in contatosParaExibir)
+                {
+                    ContatosListBox.Items.Add(contato);
+                }
+            }
+        }
+
+        private void LimparCampos()
+        {
+            NomeTextBox.Clear();
+            TelefoneTextBox.Clear();
+            EmailTextBox.Clear();
+            TipoContatoComboBox.SelectedIndex = -1;
+        }
+
+        // Método para carregar contatos quando o formulário é carregado
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Carregar contatos do armazenamento
+            contatos = CarregarContatosDoArmazenamento();
+
+            // Atualizar a lista de contatos
+            AtualizarListBox();
+        }
+
+        // Método para salvar os contatos no armazenamento permanente
+        private void SalvarContatosNoArmazenamento()
+        {
+            // Converte cada contato para uma string formatada para salvar no arquivo
+            List<string> linhas = contatos.Select(contato => $"{contato.Nome},{contato.Telefone},{contato.Email},{contato.Tipo}").ToList();
+
+            // Salva as linhas no arquivo de texto
+            File.WriteAllLines("contatos.txt", linhas);
+        }
+
+        // Método para carregar contatos do armazenamento permanente
+        private List<Contato> CarregarContatosDoArmazenamento()
+        {
+            List<Contato> contatosCarregados = new List<Contato>();
+
+            // Verifica se o arquivo de contatos existe
+            if (File.Exists("contatos.txt"))
+            {
+                // Carrega todas as linhas do arquivo
+                string[] linhas = File.ReadAllLines("contatos.txt");
+
+                // Para cada linha, divide os valores pelo separador ',' e cria um novo contato
+                foreach (string linha in linhas)
+                {
+                    string[] valores = linha.Split(',');
+                    if (valores.Length == 4)
+                    {
+                        Contato contato = new Contato(valores[0], valores[1], valores[2], valores[3]);
+                        contatosCarregados.Add(contato);
+                    }
+                }
+            }
+
+            return contatosCarregados;
+        }
+    }
+
+    public class Contato
+    {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Tipo { get; set; }
+
+        public Contato(string nome, string telefone, string email, string tipo)
+        {
+            Nome = nome;
+            Telefone = telefone;
+            Email = email;
+            Tipo = tipo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Telefone} - {Email} - {Tipo}";
+        }
+    }
+}
